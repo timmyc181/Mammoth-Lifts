193f762c6763c8cdc459bd60d8f5f75cc1eb3099
Timmy Cleveland <timothy.j.cleveland1@gmail.com>
2023-07-07 18:07:11 -0400

    Initial Commit

---
+++ Mammoth LiftsUITests/Mammoth_LiftsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mammoth_LiftsUITestsLaunchTests.swift
+//  Mammoth LiftsUITests
+//
+//  Created by Timothy Cleveland on 7/7/23.
+//
+
+import XCTest
+
+final class Mammoth_LiftsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
