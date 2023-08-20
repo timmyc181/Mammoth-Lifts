070736ad35c76c8a877b1f5191c66c911146d876
Timmy Cleveland <timothy.j.cleveland1@gmail.com>
2023-08-20 12:40:43 -0400

    Log lift, start of edit lift/lift details

---
+++ Mammoth Lifts/Views/Components/DateTimeSelector.swift
@@ -0,0 +1,13 @@
+//
+
+import SwiftUI
+
+struct DateTimeSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateTimeSelector()
+}
--- Mammoth Lifts/Views/Lifts/Edit lift/EditLiftHeaderView.swift
+++ Mammoth Lifts/Views/Lifts/Edit lift/EditLiftHeaderView.swift
@@ -2,12 +2,12 @@
 
 import SwiftUI
 
-struct DeleteListItemView: View {
+struct EditLiftHeaderView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DeleteListItemView()
+    EditLiftHeaderView()
 }
