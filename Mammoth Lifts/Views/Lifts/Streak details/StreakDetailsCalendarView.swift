32ec14764dc5792c0efc1904a618f72d1ab06cfe
Timmy Cleveland <timothy.j.cleveland1@gmail.com>
2023-12-01 22:02:55 -0500

    streak calendar, notifications

---
+++ Mammoth Lifts/Views/Lifts/Streak details/StreakDetailsCalendarView.swift
@@ -0,0 +1,13 @@
+//
+
+import SwiftUI
+
+struct StreakDetailsCalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StreakDetailsCalendarView()
+}
